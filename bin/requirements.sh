252ff03f841dac1481f2f6710a37f1f9a131b84e
shuvam021 <career.shuvam@gmail.com>
2022-06-05 23:33:36 +0530

    [shuvam021] Added bin folder

---
+++ bin/requirements.sh
@@ -0,0 +1,8 @@
+#django and plugins
+pip install django djangorestframework djangorestframework-simplejwt
+
+#utils
+pip install pydantic[dotenv] drf-yasg Pillow
+
+#database
+pip install psycopg2-binary
